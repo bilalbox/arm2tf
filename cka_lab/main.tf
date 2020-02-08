590c701f5ca4e34acc098f85048d6b8c189a9060
Nasir <bilalbox@gmail.com>
2020-02-09 07:36:13 +0800

    adding support for availability sets to tf

--- cka_lab/main.tf
+++ cka_lab/main.tf
@@ -1,158 +1,167 @@
 provider "azurerm" {
     subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    tenant_id       = var.tenant_id
-    version         = "=1.39.0"
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
+    version = "=1.39.0"
 }
 
 resource "azurerm_resource_group" "cka-lab" {
-  name     = var.resource_group
+  name = var.resource_group
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  location            = azurerm_resource_group.cka-lab.location
-  address_space       = [var.address_space]
+  name = var.virtual_network_name
+  location = azurerm_resource_group.cka-lab.location
+  address_space = [var.address_space]
   resource_group_name = azurerm_resource_group.cka-lab.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.prefix}-subnet"
+  name = "${var.prefix}-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.cka-lab.name
-  address_prefix       = var.subnet_prefix
+  resource_group_name = azurerm_resource_group.cka-lab.name
+  address_prefix = var.subnet_prefix
 }
 
 resource "azurerm_network_security_group" "cka-lab-sg" {
-  name                = "${var.prefix}-sg"
-  location            = var.location
+  name = "${var.prefix}-sg"
+  location = var.location
   resource_group_name = azurerm_resource_group.cka-lab.name
 
   security_rule {
-    name                       = "HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = var.source_network
+    name = "HTTP"
+    priority = 100
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = var.source_network
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "SSH"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = var.source_network
+    name = "SSH"
+    priority = 101
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = var.source_network
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "HTTPS"
-    priority                   = 102
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = var.source_network
+    name = "HTTPS"
+    priority = 102
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "443"
+    source_address_prefix = var.source_network
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "K8SAPI"
-    priority                   = 103
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "6443"
-    source_address_prefix      = var.source_network
+    name = "K8SAPI"
+    priority = 103
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "6443"
+    source_address_prefix = var.source_network
     destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_network_interface" "cka-lab-nic-master" {
-  count                     = var.cluster_size.masters
-  name                      = "${var.prefix}-nic-master-${count.index}"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.cka-lab.name
+resource "azurerm_network_interface" "cka-lab-nic-controller" {
+  count = var.cluster_size.controllers
+  name = "${var.prefix}-nic-controller-${count.index}"
+  location = var.location
+  resource_group_name = azurerm_resource_group.cka-lab.name
   network_security_group_id = azurerm_network_security_group.cka-lab-sg.id
 
   ip_configuration {
-    name                          = "${var.prefix}-ipconfig"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name = "${var.prefix}-ipconfig"
+    subnet_id = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.cka-lab-pip-master[count.index].id
+    public_ip_address_id = azurerm_public_ip.cka-lab-pip-controller[count.index].id
   }
 }
 
-resource "azurerm_public_ip" "cka-lab-pip-master" {
-  count               = var.cluster_size.masters
-  name                = "${var.prefix}-master-pip-${count.index}"
-  location            = var.location
+resource "azurerm_public_ip" "cka-lab-pip-controller" {
+  count = var.cluster_size.controllers
+  name = "${var.prefix}-controller-pip-${count.index}"
+  location = var.location
   resource_group_name = azurerm_resource_group.cka-lab.name
-  allocation_method   = "Dynamic"
-  domain_name_label   = "${var.prefix}-master-${count.index}"
+  allocation_method = "Dynamic"
+  domain_name_label = "${var.prefix}-controller-${count.index}"
 }
 
 resource "azurerm_network_interface" "cka-lab-nic-worker" {
-  count                     = var.cluster_size.workers
-  name                      = "${var.prefix}-nic-worker-${count.index}"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.cka-lab.name
+  count = var.cluster_size.workers
+  name = "${var.prefix}-nic-worker-${count.index}"
+  location = var.location
+  resource_group_name = azurerm_resource_group.cka-lab.name
   network_security_group_id = azurerm_network_security_group.cka-lab-sg.id
 
   ip_configuration {
-    name                          = "${var.prefix}-ipconfig"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name = "${var.prefix}-ipconfig"
+    subnet_id = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.cka-lab-pip-worker[count.index].id
+    public_ip_address_id = azurerm_public_ip.cka-lab-pip-worker[count.index].id
   }
 }
 
 resource "azurerm_public_ip" "cka-lab-pip-worker" {
-  count               = var.cluster_size.workers
-  name                = "${var.prefix}-worker-pip-${count.index}"
-  location            = var.location
+  count = var.cluster_size.workers
+  name = "${var.prefix}-worker-pip-${count.index}"
+  location = var.location
   resource_group_name = azurerm_resource_group.cka-lab.name
-  allocation_method   = "Dynamic"
-  domain_name_label   = "${var.prefix}-worker-${count.index}"
+  allocation_method = "Dynamic"
+  domain_name_label = "${var.prefix}-worker-${count.index}"
 }
 
-resource "azurerm_virtual_machine" "master" {
-  count               = var.cluster_size.masters
-  name                = "${var.prefix}-master-${count.index}"
-  location            = var.location
+resource "azurerm_availability_set" "aset_cka_lab" {
+  name = var.aset_name
+  location = var.location
   resource_group_name = azurerm_resource_group.cka-lab.name
-  vm_size             = var.vm_size
+  platform_fault_domain_count = 2
+  platform_update_domain_count = 2
+}
 
-  network_interface_ids         = [azurerm_network_interface.cka-lab-nic-master[count.index].id]
+resource "azurerm_virtual_machine" "controller" {
+  count = var.cluster_size.controllers
+  name = "${var.prefix}-controller-${count.index}"
+  location = var.location
+  resource_group_name = azurerm_resource_group.cka-lab.name
+  vm_size = var.vm_size
+
+  availability_set_id = azurerm_availability_set.aset_cka_lab.id
+  network_interface_ids = [azurerm_network_interface.cka-lab-nic-controller[count.index].id]
   delete_os_disk_on_termination = "true"
 
   storage_image_reference {
     publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = var.image_version
+    offer = var.image_offer
+    sku = var.image_sku
+    version = var.image_version
   }
 
   storage_os_disk {
-    name              = "${var.prefix}-master-${count.index}-osdisk"
+    name = "${var.prefix}-controller-${count.index}-osdisk"
     managed_disk_type = "StandardSSD_LRS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
+    caching = "ReadWrite"
+    create_option = "FromImage"
   }
 
   os_profile {
-    computer_name  = "${var.prefix}-master-${count.index}"
+    computer_name = "${var.prefix}-controller-${count.index}"
     admin_username = var.admin_username
   }
 
@@ -165,13 +174,13 @@ resource "azurerm_virtual_machine" "master" {
   }
 
   provisioner "file" {
-    source      = "files/node-install.sh"
+    source = "files/node-install.sh"
     destination = "/home/${var.admin_username}/setup.sh"
     connection {
-      type     = "ssh"
-      user     = var.admin_username
+      type = "ssh"
+      user = var.admin_username
       private_key = file("~/.ssh/id_rsa")
-      host     = azurerm_public_ip.cka-lab-pip-master[count.index].fqdn
+      host = azurerm_public_ip.cka-lab-pip-controller[count.index].fqdn
     }
   }
 
@@ -183,44 +192,44 @@ resource "azurerm_virtual_machine" "master" {
       "mkdir -p ~/.kube",
       "sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config",
       "sudo chmod a+r ~/.kube/config",
-      "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+      "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/controller/Documentation/kube-flannel.yml"
     ]
 
     connection {
-      type     = "ssh"
-      user     = var.admin_username
+      type = "ssh"
+      user = var.admin_username
       private_key = file("~/.ssh/id_rsa")
-      host     = azurerm_public_ip.cka-lab-pip-master[count.index].fqdn
+      host = azurerm_public_ip.cka-lab-pip-controller[count.index].fqdn
     }
   }
 }
 
 resource "azurerm_virtual_machine" "worker" {
-  count               = var.cluster_size.workers
-  name                = "${var.prefix}-worker-${count.index}"
-  location            = var.location
+  count = var.cluster_size.workers
+  name = "${var.prefix}-worker-${count.index}"
+  location = var.location
   resource_group_name = azurerm_resource_group.cka-lab.name
-  vm_size             = var.vm_size
+  vm_size = var.vm_size
 
-  network_interface_ids         = [azurerm_network_interface.cka-lab-nic-worker[count.index].id]
+  network_interface_ids = [azurerm_network_interface.cka-lab-nic-worker[count.index].id]
   delete_os_disk_on_termination = "true"
 
   storage_image_reference {
     publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = var.image_version
+    offer = var.image_offer
+    sku = var.image_sku
+    version = var.image_version
   }
 
   storage_os_disk {
-    name              = "${var.prefix}-worker-${count.index}-osdisk"
+    name = "${var.prefix}-worker-${count.index}-osdisk"
     managed_disk_type = "StandardSSD_LRS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
+    caching = "ReadWrite"
+    create_option = "FromImage"
   }
 
   os_profile {
-    computer_name  = "${var.prefix}-worker-${count.index}"
+    computer_name = "${var.prefix}-worker-${count.index}"
     admin_username = var.admin_username
   }
 
@@ -233,13 +242,13 @@ resource "azurerm_virtual_machine" "worker" {
   }
 
   provisioner "file" {
-    source      = "files/node-install.sh"
+    source = "files/node-install.sh"
     destination = "/home/${var.admin_username}/setup.sh"
     connection {
-      type     = "ssh"
-      user     = var.admin_username
+      type = "ssh"
+      user = var.admin_username
       private_key = file("~/.ssh/id_rsa")
-      host     = azurerm_public_ip.cka-lab-pip-worker[count.index].fqdn
+      host = azurerm_public_ip.cka-lab-pip-worker[count.index].fqdn
     }
   }
 
@@ -247,14 +256,14 @@ resource "azurerm_virtual_machine" "worker" {
     inline = [
       "chmod +x /home/${var.admin_username}/setup.sh",
       "sudo /home/${var.admin_username}/setup.sh",
-      "sudo kubeadm join ${azurerm_network_interface.cka-lab-nic-master[0].ip_configuration[0].private_ip_address}:6443 --token ${var.kubeadm_token} --discovery-token-unsafe-skip-ca-verification"
+      "sudo kubeadm join ${azurerm_network_interface.cka-lab-nic-controller[0].ip_configuration[0].private_ip_address}:6443 --token ${var.kubeadm_token} --discovery-token-unsafe-skip-ca-verification"
     ]
 
     connection {
-      type     = "ssh"
-      user     = var.admin_username
+      type = "ssh"
+      user = var.admin_username
       private_key = file("~/.ssh/id_rsa")
-      host     = azurerm_public_ip.cka-lab-pip-worker[count.index].fqdn
+      host = azurerm_public_ip.cka-lab-pip-worker[count.index].fqdn
     }
   }
 }
--- cka_lab/variables.tf
+++ cka_lab/variables.tf
@@ -19,17 +19,22 @@ variable "kubeadm_token" {
 
 variable "resource_group" {
   description = "The name of your Azure Resource Group."
-  default     = "cka-lab-rg"
+  default = "cka-lab-rg"
+}
+
+variable "aset_name" {
+  description = "The name of the virtual machine availability set"
+  default = "aset_cka_lab_controllers"
 }
 
 variable "prefix" {
   description = "This prefix will be included in the name of some resources."
-  default     = "cka-lab"
+  default = "cka-lab"
 }
 
 variable "cluster_size" {
   description = "Virtual machine hostname. Used for local hostname, DNS, and storage-related names."
-  default     = {
+  default = {
     masters = 1,
     workers = 2
   }
@@ -37,71 +42,71 @@ variable "cluster_size" {
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "eastasia"
+  default = "eastasia"
 }
 
 variable "virtual_network_name" {
   description = "The name for your virtual network."
-  default     = "vnet"
+  default = "vnet"
 }
 
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "subnet_prefix" {
   description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+  default = "10.0.10.0/24"
 }
 
 variable "storage_account_tier" {
   description = "Defines the storage tier. Valid options are Standard and Premium."
-  default     = "Standard"
+  default = "Standard"
 }
 
 variable "storage_replication_type" {
   description = "Defines the replication type to use for this storage account. Valid options include LRS, GRS etc."
-  default     = "LRS"
+  default = "LRS"
 }
 
 variable "vm_size" {
   description = "Specifies the size of the virtual machine."
-  default     = "Standard_B2s"
+  default = "Standard_B2s"
 }
 
 variable "image_publisher" {
   description = "Name of the publisher of the image (az vm image list)"
-  default     = "Canonical"
+  default = "Canonical"
 }
 
 variable "image_offer" {
   description = "Name of the offer (az vm image list)"
-  default     = "UbuntuServer"
+  default = "UbuntuServer"
 }
 
 variable "image_sku" {
   description = "Image SKU to apply (az vm image list)"
-  default     = "16.04-LTS"
+  default = "16.04-LTS"
 }
 
 variable "image_version" {
   description = "Version of the image to apply (az vm image list)"
-  default     = "latest"
+  default = "latest"
 }
 
 variable "admin_username" {
   description = "Administrator user name"
-  default     = "cka-admin"
+  default = "cka-admin"
 }
 
 variable "ssh_key" {
   description = "SSH Public Key Data"
-  default     = ""
+  default = ""
 }
 
 variable "source_network" {
   description = "Allow access from this network prefix. Defaults to '*'."
-  default     = "*"
+  default = "*"
 }
 
